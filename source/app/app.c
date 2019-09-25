adcd6a24caf47c58077bb0c85e3b6bbcc3ac3df7
Mariano <mfilippa77@gmail.com>
2019-09-25 00:31:26 -0700

    added functionality to app: io init, systick, scheduled, comm

--- source/app/app.c
+++ source/app/app.c
@@ -8,23 +8,70 @@
 #include "lib/comm.h"
 #include "hal/io.h"
 #include "hal/sys.h"
-#include "hal/adc.h"
+#include "hal/uart.h"
+#include "hal/halconfig.h"
 #ifdef MATLAB
 #include "matlab/wrapper.h"
 #endif
 
+// defines
+#define SYSTICK_FREQ_HZ     (1000)
+#define COMM_BUFFER_SIZE    (1000)
+
+// module structure
+struct {
+    // io configuration
+    io_config_t io[IO_CH_COUNT];
+    // comm buffer
+    uint8_t rx_buffer[COMM_BUFFER_SIZE];
+    uint8_t tx_buffer[COMM_BUFFER_SIZE];
+} app;
+
 // prototypes
 void app_led_blink(void);
 void app_comm_handler(uint32_t rx_size);
-
+void app_systick(void);
+void app_adc_trigger(void);
 
 // -----------------------------------------------------------------------------
 // init
 // -----------------------------------------------------------------------------
 uint32_t app_init(void) {
+    uint32_t error = 0;
+    uint32_t handle = 0;
 
-    // success
-    return 0;
+    // disable all interrupts
+    sys_disable_interrupts();
+
+    // init systick
+    error |= sys_tick_init(SYSTICK_FREQ_HZ, &app_systick);
+
+    // init gpio
+    app.io[IO_LED].port = 0; 
+    app.io[IO_LED].pin = 0; 
+    app.io[IO_LED].type = 1; 
+    app.io[IO_LED].ah = 1; 
+    app.io[IO_LED].state = 0;
+    app.io[IO_DEBUG].port = 0; 
+    app.io[IO_DEBUG].pin = 1; 
+    app.io[IO_DEBUG].type = 1; 
+    app.io[IO_DEBUG].ah = 1; 
+    app.io[IO_DEBUG].state = 0;
+    error |= io_init(app.io, IO_CH_COUNT);
+
+    // init scheduler
+    error |= sch_init();
+    handle = sch_function_register(&app_led_blink,500);
+    if (handle==0) error = 1;
+
+    // init comm
+    error |= comm_init(UART0, COMM_BUFFER_SIZE, app.rx_buffer, app.tx_buffer,
+        &app_comm_handler);
+
+    // if no errors, enable interrupts
+    if (!error) sys_enable_interrupts();
+
+    return error;
 }
 
 // -----------------------------------------------------------------------------
@@ -49,23 +96,17 @@ void app_systick(void) {
 // led blink
 // -----------------------------------------------------------------------------
 void app_led_blink(void) {
-    io_toggle(0);
+    io_toggle(IO_LED);
 }
 
-// -----------------------------------------------------------------------------
-// dummy
-// -----------------------------------------------------------------------------
-void app_dummy(void) {
-
-}
 // -----------------------------------------------------------------------------
 // adc trigger
 // -----------------------------------------------------------------------------
 void app_adc_trigger(void) {
     // trigger fast conversion
-    adc_sw_trigger_fast();
+    // adc_sw_trigger_fast();
     // trigger slow conversion
-    adc_sw_trigger_slow();
+    // adc_sw_trigger_slow();
 }
 
 // -----------------------------------------------------------------------------
--- source/hal/halconfig.h
+++ source/hal/halconfig.h
@@ -7,9 +7,8 @@
 
 // io channels
 enum io_channel_enum {
-    IO_LED = 0,            // LED
-    IO_DEBUG1,            // DEBUG1
-    IO_DEBUG2,            // DEBUG2
+    IO_LED = 0,           // LED
+    IO_DEBUG,             // DEBUG
     IO_CH_COUNT,
 };
 
@@ -20,20 +19,11 @@ enum adc_channels_enum {
     ADC_FAST_CH2,
     ADC_FAST_CH3,
     ADC_FAST_CH4,
-    ADC_FAST_CH5,
-    ADC_FAST_CH6,
-    ADC_FAST_CH7,
-    ADC_FAST_CH8,
-    ADC_FAST_CH9,
-    ADC_FAST_CH10,
     // slow channels
     ADC_SLOW_CH1,
     ADC_SLOW_CH2,
     ADC_SLOW_CH3,
     ADC_SLOW_CH4,
-    ADC_SLOW_CH5,
-    ADC_SLOW_CH6,
-    ADC_SLOW_CH7,
     ADC_SLOW_TEMP,
     ADC_CH_COUNT,
 };
@@ -45,20 +35,11 @@ enum fb_channel_enum {
     FB_FAST_CH2,
     FB_FAST_CH3,
     FB_FAST_CH4,
-    FB_FAST_CH5,
-    FB_FAST_CH6,
-    FB_FAST_CH7,
-    FB_FAST_CH8,
-    FB_FAST_CH9,
-    FB_FAST_CH10,
     // slow channels
     FB_SLOW_CH1,
     FB_SLOW_CH2,
     FB_SLOW_CH3,
     FB_SLOW_CH4,
-    FB_SLOW_CH5,
-    FB_SLOW_CH6,
-    FB_SLOW_CH7,
     FB_TEMP,
     FB_CH_COUNT,
 };
